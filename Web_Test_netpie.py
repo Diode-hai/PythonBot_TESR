cc8d83865ce27c0f02e221562f5a91c29d764e0a
Diode Meehai <ponza.zaza11@gmail.com>
2018-08-14 17:19:53 +0700

    Update Web_Test_netpie.py

--- Web_Test_netpie.py
+++ Web_Test_netpie.py
@@ -3,7 +3,27 @@ from flask import Flask, request, abort
 import requests # pip install requests
 import urllib3
 
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+
 app = Flask(__name__)
+
+#Token
+line_bot_api = LineBotApi('uPPbuNIIATryuuhKwEC4kRRHqdNd4+73PnMi+7DQ383G4NMkI6S7a+li5SCoqZJGbKsl3sxQ6J556VApXNkprhB4dKVg1rR6UcHZzSTFyte5h0qyqFAb9zISLsL5LEw+DS/KlnXV/IG5UDYV3g6qjQdB04t89/1O/w1cDnyilFU=')
+#Channel secret
+handler = WebhookHandler('bce117e84ff9f7e593d428b1ffac7aa4')
+
+APPID="BotChatLine"
+KEY = "me37I8KsiCqpTWS"
+SECRET = "ozPtNGTK6GPA1STe1PjIvvrwS"
+Topic = "/LED_Control"
  
 @app.route("/")
 def hello():
