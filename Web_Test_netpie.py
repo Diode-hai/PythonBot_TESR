7d3e0df70b4be958b7fdd43647ec61e6a61562e0
Diode Meehai <ponza.zaza11@gmail.com>
2018-08-14 17:58:01 +0700

    Update Web_Test_netpie.py

--- Web_Test_netpie.py
+++ Web_Test_netpie.py
@@ -39,6 +39,20 @@ def hello():
     return "Hello World!"
     
 #----------------------------------------------------
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
 	#global url , KEY , SECRET
