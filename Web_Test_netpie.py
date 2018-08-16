fb8b501637d2fdd37768fc4a1f82697f9912beed
anoney <ponza.zaza11@gmail.com>
2018-08-16 13:03:56 +0700

    Update Web_Test_netpie.py

--- Web_Test_netpie.py
+++ Web_Test_netpie.py
@@ -10,7 +10,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage, ImageSendMessage
 )
 
 app = Flask(__name__)
@@ -64,10 +64,16 @@ def handle_message(event):
     	r = requests.put(url, data = {'':'ON'} , auth=(str(KEY),str(SECRET)))
 		
     elif "off" in str(event.message.text):
-    	line_bot_api.reply_message(event.reply_token,TextSendMessage(text='OFF LED'))
+    	#line_bot_api.reply_message(event.reply_token,TextSendMessage(text='OFF LED'))
 
     	#REST API NETPIE OFF LED
     	r = requests.put(url, data = {'':'OFF'} , auth=(str(KEY),str(SECRET)))
+	
+	message = ImageSendMessage(
+    		original_content_url='https://example.com/original.jpg',
+    		preview_image_url='https://example.com/preview.jpg'
+	)
+	line_bot_api.reply_message(event.reply_token, message)
 
     elif "temp?" in str(event.message.text):
     	#REST API NETPIE read sensor value
